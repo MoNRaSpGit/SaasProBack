4dd64c04927bc019582b27957dca3d3bb6f37a11
MoNRaSpGit <ju4nrsuarez@gmail.com>
2026-04-28 10:31:52 -0300

    fix(api): enable CORS for local and github pages

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
+      "https://monraspgit.github.io"
+    ]
+  });
   app.setGlobalPrefix("api/v1");
   app.useGlobalPipes(
     new ValidationPipe({
